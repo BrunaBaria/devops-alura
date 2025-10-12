9529a2ac7980b32b4debd35142218f9d4b8c3791
Bruna Baria <bruna.baria.m@gmail.com>
2025-10-12 13:16:20 -0300

    conclusão da sprint 2

--- 02-sprint/logica-programacao-funcoes-listas/app.js
+++ 02-sprint/logica-programacao-funcoes-listas/app.js
@@ -4,20 +4,22 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Descubra o número secreto entre 1 e 10!';
 
-let numeroAleatorio = gerarNumeroAleatorio();
-let tentativas = 1;
+let tamanhoDoJogo = 3; //define o tamanho do jogo, ou seja, o maior número possível
+let listaDeNumerosSortiados = []; //array para armazenar os números já sorteados
+let numeroAleatorio = gerarNumeroAleatorio(); //número aleatório entre 1 e tamanhoDoJogo
+let tentativas = 1; //contador de tentativas
 
-function exibirTextoNaTela(tag,texto){
+function exibirTextoNaTela(tag,texto){ //função para exibir texto na tela
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-function exibirMensagemInicial(){
+function exibirMensagemInicial(){ //função para exibir a mensagem inicial
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Descubra o número secreto entre 1 e 10!');
+    exibirTextoNaTela('p', `Descubra o número secreto entre 1 e ${tamanhoDoJogo}!`);
 }
 
-exibirMensagemInicial();
+exibirMensagemInicial(); //chamada da função para exibir a mensagem inicial
 
 function verificarChute(){
     let chute = parseInt(document.querySelector('input').value);
@@ -37,7 +39,19 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10) + 1;
+    let numeroEscolhido = parseInt(Math.random() * tamanhoDoJogo) + 1;
+    let quantidadeDeElementosDaLista = listaDeNumerosSortiados.length //verifica a quantidade de elementos no array
+    if(quantidadeDeElementosDaLista >= tamanhoDoJogo){ //se a quantidade de elementos for igual ao tamanho do jogo, todos os números já foram sorteados
+        listaDeNumerosSortiados = []; //limpa o array
+    }
+    
+    if(listaDeNumerosSortiados.includes(numeroEscolhido)){ //a função includes verifica se o array já contém o número
+        return gerarNumeroAleatorio(); //chamada recursiva - a função se chama novamente
+    }else{
+        listaDeNumerosSortiados.push(numeroEscolhido); //adiciona o número ao array
+        console.log(listaDeNumerosSortiados);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo(){
@@ -52,3 +66,5 @@ function reiniciarJogo(){
     tentativas = 1;
     document.getElementById('reiniciar').setAttribute('disabled', 'true');
 }
+
+
